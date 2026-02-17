f1b14c4dcfa739caa15c17a055ba0be76a1f14de
Christy <christyxavier777@gmail.com>
2026-02-17 22:52:56 +0530

    frontend: wrap dashboard routes with ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Register from './pages/Register'
 import AdminDashboard from './pages/AdminDashboard'
 import TrainerDashboard from './pages/TrainerDashboard'
 import MemberDashboard from './pages/MemberDashboard'
+import ProtectedRoute from './components/ProtectedRoute'
 import './App.css'
 
 function App() {
@@ -12,9 +13,30 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/trainer" element={<TrainerDashboard />} />
-        <Route path="/member" element={<MemberDashboard />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/trainer"
+          element={
+            <ProtectedRoute>
+              <TrainerDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/member"
+          element={
+            <ProtectedRoute>
+              <MemberDashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   )
