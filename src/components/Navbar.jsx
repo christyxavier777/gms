1e21a78b909d5f078fa277721032959dfcf8150d
Christy <christyxavier777@gmail.com>
2026-02-18 22:35:08 +0530

    frontend: add basic navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom'
+
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <h2>Gym Management</h2>
+    <nav className="bg-blue-600 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h2 className="text-white text-xl font-bold">Gym Management</h2>
+        <ul className="flex space-x-4">
+          <li><Link to="/" className="text-white hover:text-gray-200">Home</Link></li>
+          <li><Link to="/login" className="text-white hover:text-gray-200">Login</Link></li>
+          <li><Link to="/register" className="text-white hover:text-gray-200">Register</Link></li>
+        </ul>
       </div>
-      <ul className="navbar-menu">
-        <li><a href="/">Home</a></li>
-        <li><a href="/login">Login</a></li>
-      </ul>
     </nav>
   );
 }
